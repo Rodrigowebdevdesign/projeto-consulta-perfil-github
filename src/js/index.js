dac9466ac2539f0e57736b83493d26040e68746e
Rodrigo Oliveira <rodrigowebdev-design@gmail.com>
2023-04-23 12:11:37 -0300

    html e css inicial e conexão com a api feitos com sucesso

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+async function user(userName){
+    const response = await fetch(`https://api.github.com/users/${userName}`);
+    return await response.json();
+};
+function getUserProfile(userName){
+    user(userName).then(userData =>{
+        let userInfo = `<img src="${userData.avatar_url}" alt="foto do perfil do usuário"
+                        <div class="data">
+                            <h1>${userData.name ?? 'Usuário não possui um nome cadastrado 😢'}</h1>
+                            <p>${userData.bio ?? 'Usuário não possui uma biografia cadastrada 😢'}</p>
+                        </div> `
+
+            document.querySelector('.profile-data').innerHTML = userInfo;
+    });
+};
+getUserProfile('rodrigowebdev-design')
